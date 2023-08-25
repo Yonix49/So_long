2e57a67afc8e0eb233c2d09cf87477dba5fc8f4a
Mohammed HAJJI BABA <mhajji-b@paul-f4Br3s1.clusters.42paris.fr>
2023-08-25 17:05:26 +0200

    ⚡️ Working progress🔨🔥

--- SRCS/so_long.c
+++ SRCS/so_long.c
@@ -6,20 +6,20 @@
 /*   By: mhajji-b <mhajji-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:08:29 by mhajji-b          #+#    #+#             */
-/*   Updated: 2023/03/06 16:50:10 by mhajji-b         ###   ########.fr       */
+/*   Updated: 2023/08/25 17:04:20 by mhajji-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "string.h"
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **env)
 {
 	char	**tmp;
 	t_data	*img;
 
-	if (verif_namearg(ac, av) == 0 || verif_open(av) == 0)
-		return (0);
+	if (verif_namearg(ac, av) == 0 || verif_open(av) == 0 || (env[0] == NULL))
+		return (4);
 	img = (t_data *)malloc(sizeof(t_data));
 	if (!img)
 		return (0);
@@ -27,14 +27,14 @@ int	main(int ac, char **av)
 	if (img->map == NULL)
 	{
 		free(img);
-		return (0);
+		return (5);
 	}
 	tmp = copy_map(img);
 	if (verifall(tmp) == 0)
 	{
 		ft_free_all(img);
 		ft_free(tmp);
-		return (0);
+		return (3);
 	}
 	ft_free(tmp);
 	if (display_window(img) == -1)
